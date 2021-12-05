badeafaede0bf01107760be328a516f6e749f6e6
Rinzzz <1164699978@qq.com>
2021-12-05 19:24:37 +0800

    new_item改变

--- app/src/main/java/com/example/myapplication/adapter/NewsAdapter.java
+++ app/src/main/java/com/example/myapplication/adapter/NewsAdapter.java
@@ -33,7 +33,7 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.news_item2, parent , false);
+                .inflate(R.layout.news_item, parent , false);
         ViewHolder holder = new ViewHolder(view);
         return holder;
 
